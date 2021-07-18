5f74c08a7209e6acbc97ecba3b5af1b8dd50279b
Barooj-Un-Nisa <87430872+Barooj-Un-Nisa@users.noreply.github.com>
2021-07-18 13:13:35 +0530

    Added my own header file

---
+++ myheader.h
@@ -0,0 +1,23 @@
+void add(int a,int b){
+	int sum = 0;
+	sum = a + b;
+	printf("Sum is %d\n",sum);
+}
+
+void subtract(int a,int b){
+	int difference = 0;
+	difference = a - b;
+	printf("%d\n",difference);
+}
+
+void multiply(int a,int b){
+	int product = 1;
+	product = a * b;
+	printf("%d",product);
+}
+
+void division(int a,int b){
+	float result = 0;
+	result = (float)a / b;
+	printf("%d",result);
+}
