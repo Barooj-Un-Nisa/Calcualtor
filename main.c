99cb7989836b437d82465c953fbc0952007f2a1d
Barooj-Un-Nisa <87430872+Barooj-Un-Nisa@users.noreply.github.com>
2021-07-19 10:48:23 +0530

    Changed from if else to switch case

--- main.c
+++ main.c
@@ -1,34 +1,56 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #include"myheader.h"
 
 int main(){
-	int num1,num2;
-	printf("Enter first number:");
-	scanf("%d",&num1);
-	printf("Enter second number:");
-	scanf("%d",&num2);
-	int n;
-	printf("Enter 1 for Addition");
-	printf("\nEnter 2 for Subtraction");
-	printf("\nEnter 3 for Multiplication");
-	printf("\nEnter 4 for Division\n");
-	scanf("%d",&n);
-	if(n == 1){
-		int sum = add(num1,num2);
-		printf("Sum is %d\n",sum);
-	}
-	else if(n == 2){
-		int difference = subtract(num1,num2);
-		printf("Difference is %d\n",difference); 
-	}
-	else if(n == 3){
-		int product = multiply(num1,num2);
-		printf("Product is %d",product);
-	}
-	else{
-		float result = division(num1,num2);
-		printf("Quotient is %f",result);
+	int num1,num2,n;
+	int sum = 0,difference = 0, product = 1;
+	float  result = 0;
+	while(1){
+		printf("Enter 1 for Addition");
+		printf("\nEnter 2 for Subtraction");
+		printf("\nEnter 3 for Multiplication");
+		printf("\nEnter 4 for Division\n");
+		printf("Enter 5 for exit\n");
+		scanf("%d",&n);
+		
+		switch(n){
+			case 1 :
+				printf("Enter first number:");
+				scanf("%d",&num1);
+				printf("Enter second number:");
+				scanf("%d",&num2);
+				sum = add(num1,num2);
+				printf("Sum is %d\n",sum);
+				break;
+			case 2:
+				printf("Enter first number:");
+				scanf("%d",&num1);
+				printf("Enter second number:");
+				scanf("%d",&num2);
+				difference = subtract(num1,num2);
+				printf("Difference is is %d\n",difference);
+				break;
+			case 3:
+				printf("Enter first number:");
+				scanf("%d",&num1);
+				printf("Enter second number:");
+				scanf("%d",&num2);
+				product = multiply(num1,num2);
+				printf("Product is %d\n",product);
+				break;
+			case 4 :
+				printf("Enter first number:");
+				scanf("%d",&num1);
+				printf("Enter second number:");
+				scanf("%d",&num2);
+				result = division(num1,num2);
+				printf("Result is %f\n",result);
+				break;
+			case 5:
+				exit(0);
+		}
 	}
 	return 0;
 }
